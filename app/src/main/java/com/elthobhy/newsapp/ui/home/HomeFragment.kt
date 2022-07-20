e0f80d0a2d7229e0d0c62f182927e20fcf30e061
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-07-20 11:12:02 +0700

    View Model using dummy data - Horizontal RecyclerView

--- app/src/main/java/com/elthobhy/newsapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/elthobhy/newsapp/ui/home/HomeFragment.kt
@@ -15,6 +15,7 @@ import com.elthobhy.newsapp.R
 import com.elthobhy.newsapp.data.source.local.entity.Article
 import com.elthobhy.newsapp.databinding.FragmentHomeBinding
 import com.elthobhy.newsapp.utils.loadingExtension
+import com.elthobhy.newsapp.viewmodel.EverythingViewModel
 import com.elthobhy.newsapp.viewmodel.HeadlineViewModel
 
 class HomeFragment : Fragment() {
@@ -22,17 +23,42 @@ class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
     private val binding get() = _binding as FragmentHomeBinding
     private lateinit var adapterHeadline: HeadlineAdapter
+    private lateinit var adapterEverything: EverythingAdapter
     private lateinit var headlineViewModel: HeadlineViewModel
+    private lateinit var everythingViewModel: EverythingViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         headlineViewModel = ViewModelProvider(this).get(HeadlineViewModel::class.java)
+        everythingViewModel = ViewModelProvider(this).get(EverythingViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater,container,false)
         adapterHeadline = HeadlineAdapter()
+        adapterEverything  = EverythingAdapter()
         showRvHeadline()
+        showRvEverything()
         return binding.root
+    }
+
+    private fun showRvEverything() {
+        adapterEverything.notifyDataSetChanged()
+        binding.rvBusiness.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
+            setHasFixedSize(true)
+            adapter = adapterEverything
+        }
+        val everything = everythingViewModel.getEverything()
+        false.loadingExtension(binding.shimmerEverything,binding.rvBusiness)
+        adapterEverything.setList(everything)
+        Log.e("debug", "showRvEverything: $everything", )
+
+        adapterEverything.setOnClickCallback(object :EverythingAdapter.OnItemClickCallback{
+            override fun onClicked(data: Article) {
+                Toast.makeText(context,"cliked",Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     private fun showRvHeadline() {

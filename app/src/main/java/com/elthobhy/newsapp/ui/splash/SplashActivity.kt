77a8f34fa86f01547dce8f2ca395eec357950d9c
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-07-24 08:10:32 +0700

    Adding Splash Screen

---
+++ app/src/main/java/com/elthobhy/newsapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.elthobhy.newsapp.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.elthobhy.newsapp.R
+import com.elthobhy.newsapp.databinding.ActivitySplashBinding
+import com.elthobhy.newsapp.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        goToMain()
+    }
+
+    private fun goToMain() {
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                startActivity(Intent(this,MainActivity::class.java))
+                finishAffinity()
+            },2000)
+    }
+}
